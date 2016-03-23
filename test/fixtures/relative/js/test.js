bb2dc28d5d9eecdd9816a14de857ddab124770eb
Burak Yigit Kaya <byk@disqus.com>
2016-03-23 14:23:19 +0200

    Throw exception on unresolved `geturl` calls in JS

--- test/fixtures/relative/js/test.js
+++ test/fixtures/relative/js/test.js
@@ -1,4 +1,4 @@
 var letsuse = geturl('../styles/test.css'),
     and = myglobal.geturl('../images/logo.png');
 
-myglobal.helpersandstuff.geturl.call(window, 'ex' + 'tern' + 'al.' + 'html', 'random', 'strings');
+myglobal.helpersandstuff.geturl.call(window, '../' + 'styles/' + 'test.' + 'css', 'random', 'strings');
